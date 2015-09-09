d5527d39545e3ff154da687ec95d82e948d9098b
Seth Vernon <seth.vernon@gmail.com>
2015-09-08 22:02:30 -0700

    Create CircleViewController.h

---
+++ CircleViewController.h
@@ -0,0 +1,15 @@
+//
+//  CircleViewController.h
+//  Circle Of Fifths
+//
+//  Created by Seth A. Vernon on 5/1/12.
+//	Copyright (c) 2012 Seth A. Vernon. All rights reserved.//
+
+
+#import <UIKit/UIKit.h>
+
+@class CircleViewControllerLandscape;
+
+@interface CircleViewController : UIViewController
+
+@end
