05e3f081f11938587cf2ef2198d4f75e15deae96
Seth Vernon <seth.vernon@gmail.com>
2015-09-08 22:00:56 -0700

    Create CircleAppDelegate.h

---
+++ CircleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CircleAppDelegate.h
+//  Circle Of Fifths
+//
+//  Created by Seth Vernon on 5/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
